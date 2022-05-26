9477c7716ce33efb4024fde34fbdce151ef9bd5e
Heng Lu <henglu@microsoft.com>
2022-05-26 14:36:58 +0800

    simplify test config

--- internal/langserver/handlers/testdata/TestValidation_commaAfterArrayItem/main.tf
+++ internal/langserver/handlers/testdata/TestValidation_commaAfterArrayItem/main.tf
@@ -1,27 +1,9 @@
 resource "azapi_resource" "diagnostics" {
   type      = "microsoft.insights/diagnosticSettings@2017-05-01-preview"
-  name      = local.diagnosticsName
+  name      = "somename"
   parent_id = azapi_resource.automationAccount.id
-
-  count = (var.enableDiagnostics) ? 1 : 0
   body = jsonencode({
     properties = {
-      workspaceId      = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.logAnalyticsResourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${var.logAnalyticsWorkspaceName}"
-      storageAccountId = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.diagnosticStorageAccountResourceGroup}/providers/Microsoft.Storage/storageAccounts/${var.diagnosticStorageAccountName}"
-      logs = [
-        {
-          category = "JobLogs"
-          enabled  = true
-        },
-        {
-          category = "JobStreams"
-          enabled  = true
-        },
-        {
-          category = "DscNodeStatus"
-          enabled  = true
-        },
-      ]
       metrics = [
         {
           category = "AllMetrics"
