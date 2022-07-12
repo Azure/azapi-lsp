d4fb034b7db3f8338e4b63098f71e896f1b47470
Heng Lu <henglu@microsoft.com>
2022-07-12 15:44:27 +0800

    preload the first definition to speed up first completion

--- internal/azure/loader.go
+++ internal/azure/loader.go
@@ -34,6 +34,12 @@ func GetAzureSchema() *Schema {
 			return nil
 		}
 	}
+	// preload the first definition
+	for _, resource := range schema.Resources {
+		if len(resource.Definitions) > 0 {
+			_, _ = resource.Definitions[0].GetDefinition()
+		}
+	}
 	return schema
 }
 
