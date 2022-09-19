7d3d0cf491e8b21faa245434285d34e2d25a998b
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2022-09-19 14:38:16 +0800

    update release pipeline (#58)

--- version.go
+++ version.go
@@ -6,8 +6,8 @@ import (
 	goversion "github.com/hashicorp/go-version"
 )
 
-// The main version number that is being run at the moment.
-var version = "0.6.0"
+// To set this from outside, use go build -ldflags "-X 'main.version=$(VERSION)'"
+var version string = "dev"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
