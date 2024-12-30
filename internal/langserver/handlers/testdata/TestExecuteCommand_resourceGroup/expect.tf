41691ef9490843061dde70d3570b389ffe6d7297
Heng Lu <henglu@microsoft.com>
2024-12-30 10:35:31 +0800

    fix a test

--- internal/langserver/handlers/testdata/TestExecuteCommand_resourceGroup/expect.tf
+++ internal/langserver/handlers/testdata/TestExecuteCommand_resourceGroup/expect.tf
@@ -1,17 +1,17 @@
 /*
 Note: This is a generated HCL content from the JSON input which is based on the latest API version available.
 To import the resource, please run the following command:
-terraform import azapi_resource.resourceGroup /subscriptions/0000000/resourceGroups/app-rg5wg5lo7sa2hxrh6bs6?api-version=2024-03-01
+terraform import azapi_resource.resourceGroup /subscriptions/0000000/resourceGroups/app-rg5wg5lo7sa2hxrh6bs6?api-version=2024-11-01
 
 Or add the below config:
 import {
-  id = "/subscriptions/0000000/resourceGroups/app-rg5wg5lo7sa2hxrh6bs6?api-version=2024-03-01"
+  id = "/subscriptions/0000000/resourceGroups/app-rg5wg5lo7sa2hxrh6bs6?api-version=2024-11-01"
   to = azapi_resource.resourceGroup
 }
 */
 
 resource "azapi_resource" "resourceGroup" {
-  type      = "Microsoft.Resources/resourceGroups@2024-03-01"
+  type      = "Microsoft.Resources/resourceGroups@2024-11-01"
   parent_id = "/subscriptions/0000000"
   name      = "app-rg5wg5lo7sa2hxrh6bs6"
   location  = "eastus"
